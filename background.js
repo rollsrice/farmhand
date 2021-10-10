f7d6cac3398c6a55b5e554655058f5cb8c98b667
rollsrice <ryanxyzhang@gmail.com>
2021-10-09 23:01:08 -0400

    parse item from explores

--- background.js
+++ background.js
@@ -3,8 +3,33 @@ var urlFilter = { urls: [target] }
 
 var rawData;
 
-function handleExploreResponse(response) {
-    console.log(response)
+async function handleExploreResponse(response) {
+    var root = document.createElement('html')
+    root.innerHTML = response
+    var items = Array.from(root.getElementsByTagName('img'))
+    items.forEach(item => {
+        parseItemUrl(item.src)
+    })
+}
+
+function parseItemUrl(itemUrl) {
+    // Get filename
+    let filename = itemUrl.split('\\').pop().split('/').pop();
+    let itemId = filename.split('.').shift()
+    let itemName = itemUrls[itemId]
+    if (itemName == null) {
+        console.log(itemId + " not added to list of items")
+    }
+    console.log(itemName)
+}
+
+let itemUrls = {
+    "5908": "Straw",
+    "mushroom": "Mushroom",
+    "6067": "Bird Egg",
+    "6143": "Wood",
+    "5910": "Arrowhead",
+    "5922": "Antler"
 }
 
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/StreamFilter/ondata
